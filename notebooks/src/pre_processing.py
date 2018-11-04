e9312db8d56ef8a221fe2eb73130b964a3c3b82f
fgsantos89 <santos.flip@gmail.com>
2018-11-04 20:51:37 -0200

    starting preprocessing optimizing the dataframe

--- notebooks/src/pre_processing.py
+++ notebooks/src/pre_processing.py
@@ -1,11 +1,22 @@
 import pandas as pd
 import numpy as np
 
-BOOL_FEATURES_MAP = {'NO': False, 'YES': True, np.nan: np.nan}
+BOOL_FEATURES_MAP = {
+    'NO': 0,
+    'YES': 1,
+    np.nan: np.nan
+}
 
 
 def tranform_boolean_features(df, columns):
-    bool_df = df[columns].applymap(lambda x: BOOL_FEATURES_MAP[x])
+    bool_df = df[columns].applymap(
+        lambda x: BOOL_FEATURES_MAP[x]).astype('bool')
     df.drop(labels=columns, axis="columns", inplace=True)
     df[columns] = bool_df[columns]
+    return df
+
+
+def tranform_int_features(df, columns):
+    df[columns] = df[columns].apply(pd.to_numeric,
+                                     downcast='integer', errors='coerce')
     return df

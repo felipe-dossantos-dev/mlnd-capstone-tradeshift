388cfc428603519d3d8224be9d1a3cb72d39d82b
fgsantos89 <santos.flip@gmail.com>
2018-11-05 22:03:08 -0200

    finish memory efficient dataframe section

--- notebooks/src/pre_processing.py
+++ notebooks/src/pre_processing.py
@@ -18,5 +18,28 @@ def tranform_boolean_features(df, columns):
 
 def tranform_int_features(df, columns):
     df[columns] = df[columns].apply(pd.to_numeric,
-                                     downcast='integer', errors='coerce')
+                                    downcast='integer', errors='coerce')
+    return df
+
+
+def tranform_float_features(df, columns):
+    df[columns] = df[columns].apply(pd.to_numeric,
+                                    downcast='float', errors='coerce')
+    return df
+
+
+def extract_uniques_words(df, columns):
+    uniques = set()
+    for c in columns:
+        uniques.update(df[c].unique().tolist())
+    return dict(zip(uniques, range(len(uniques))))
+
+
+def tranform_content_features(df, columns, uniques):
+    uniques[np.nan] = -1
+    uniques[float('nan')] = -1
+    cont_df = df[columns].applymap(
+        lambda x: uniques[x] if pd.notnull(x) else -1).astype('int32')
+    df.drop(labels=columns, axis="columns", inplace=True)
+    df[columns] = cont_df
     return df
